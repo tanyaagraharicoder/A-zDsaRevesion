32eb7e3ed8bacdc1e399a2412ddc7da716ef5273
Tanya Agrahari <rg96958227@gmail.com>
2026-02-11 01:05:53 +0530

    functionin java

---
+++ function.java
@@ -0,0 +1,11 @@
+public class function {
+    public static void main(String[] args) {
+        int result = add(5, 3);
+        System.out.println("The sum is: " + result);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    
+}
