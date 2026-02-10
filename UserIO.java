2d7314bc30d6bfcd7278c8ea6d94de29d1b91f25
Tanya Agrahari <rg96958227@gmail.com>
2026-02-10 23:25:04 +0530

    User Input Output

---
+++ UserIO.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class UserIO {
+ 
+
+
+    public static void main(String[] args) {
+         Scanner sc= new Scanner(System.in);
+    System.out.println("Enter your name: ");
+    String name = sc.nextLine();
+
+
+        System.out.println("Hello, " + name + "!");
+    }
+
+    
+}
